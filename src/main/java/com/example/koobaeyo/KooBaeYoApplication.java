516bcb6c25a5ad50136bca719d04348fea24353b
Fangsangik <hwangsangik@gmail.com>
2024-12-03 13:44:10 +0900

    setting

---
+++ src/main/java/com/example/koobaeyo/KooBaeYoApplication.java
@@ -0,0 +1,13 @@
+package com.example.koobaeyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KooBaeYoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KooBaeYoApplication.class, args);
+    }
+
+}
