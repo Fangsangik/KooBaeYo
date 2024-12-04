f3b374b3c902e24b2b09178957ff2a3b2f5c0422
FANG <107174112+Fangsangik@users.noreply.github.com>
2024-12-04 17:29:42 +0900

    feat :"ReviewController" - review 작성, 조회, 별점 조회

--- src/main/java/com/example/koobaeyo/reviews/controller/ReviewController.java
+++ src/main/java/com/example/koobaeyo/reviews/controller/ReviewController.java
@@ -1,9 +1,13 @@
 package com.example.koobaeyo.reviews.controller;
 
+import com.example.koobaeyo.common.CommonResponse;
+import com.example.koobaeyo.common.constants.Auth;
+import com.example.koobaeyo.reviews.dto.FindReviewByRateDto;
 import com.example.koobaeyo.reviews.dto.ReviewCreateResponseDto;
 import com.example.koobaeyo.reviews.dto.ReviewRequestDto;
 import com.example.koobaeyo.reviews.dto.ReviewResponseDto;
 import com.example.koobaeyo.reviews.service.ReviewService;
+import com.example.koobaeyo.user.entity.User;
 import lombok.Getter;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -17,32 +21,33 @@ public class ReviewController {
         this.reviewService = reviewService;
     }
 
-    @PostMapping("/reviews")
-    public ResponseEntity<ReviewCreateResponseDto> createReview
-            (@RequestBody ReviewRequestDto reviewRequestDto) {
-        ReviewCreateResponseDto reviewCreateResponseDto = reviewService.createReview(reviewRequestDto);
-        return ResponseEntity.ok(reviewCreateResponseDto);
+    @PostMapping("/reviews/{orderId}")
+    public ResponseEntity<CommonResponse<ReviewCreateResponseDto>> createReview
+            (@PathVariable Long orderId,
+             @SessionAttribute(Auth.LOGIN_USER) Long userId,
+             @RequestBody ReviewRequestDto reviewRequestDto) {
+        ReviewCreateResponseDto reviewCreateResponseDto = reviewService.createReview(orderId, userId, reviewRequestDto);
+        return ResponseEntity.ok(new CommonResponse<>("리뷰 생성 성공", reviewCreateResponseDto));
     }
 
     @GetMapping("/stores/{storeId}/reviews")
-    public ResponseEntity<Page<ReviewResponseDto>> getReviews
-            (@SessionAttribute("loginUser") Long userId,
-             @PathVariable Long storeId,
+    public ResponseEntity<CommonResponse<Page<ReviewResponseDto>>> getReviews
+            (@PathVariable Long storeId,
+             @SessionAttribute(Auth.LOGIN_USER) Long userId,
              @RequestParam(value = "page", required = false, defaultValue = "0") int page,
              @RequestParam(value = "size", required = false, defaultValue = "10") int size) {
         Page<ReviewResponseDto> reviewResponseDto = reviewService.findReview(storeId, userId, page, size);
-        return ResponseEntity.ok(reviewResponseDto);
+        return ResponseEntity.ok(new CommonResponse<>("리뷰 조회 성공", reviewResponseDto));
     }
 
     @GetMapping("/stores/{storeId}/reviews/rate")
-    public ResponseEntity<Page<ReviewResponseDto>> getReviewRate
+    public ResponseEntity<CommonResponse<Page<ReviewResponseDto>>> getReviewRate
             (@PathVariable Long storeId,
-             @RequestParam int minRate,
-             @RequestParam int maxRate,
+             @RequestBody FindReviewByRateDto findReviewByRateDto,
              @RequestParam(value = "page", required = false, defaultValue = "0") int page,
              @RequestParam(value = "size", required = false, defaultValue = "10") int size) {
 
-        Page<ReviewResponseDto> reviewResponseDto = reviewService.findReviewByRate(storeId, minRate, maxRate, page, size);
-        return ResponseEntity.ok(reviewResponseDto);
+        Page<ReviewResponseDto> reviewResponseDto = reviewService.findReviewByRate(storeId, findReviewByRateDto, page, size);
+        return ResponseEntity.ok(new CommonResponse<>("리뷰 조회 성공", reviewResponseDto));
     }
 }
