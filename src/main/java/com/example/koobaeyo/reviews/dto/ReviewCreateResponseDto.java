08026644628564d65a83a8649f3bd79d5bd2c1e0
FANG <107174112+Fangsangik@users.noreply.github.com>
2024-12-04 19:02:22 +0900

    refactor :"ReviewCreateResponseDto" - 사용하지 않는 메서드 삭제

--- src/main/java/com/example/koobaeyo/reviews/dto/ReviewCreateResponseDto.java
+++ src/main/java/com/example/koobaeyo/reviews/dto/ReviewCreateResponseDto.java
@@ -1,16 +1,13 @@
 package com.example.koobaeyo.reviews.dto;
 
+import com.example.koobaeyo.reviews.entity.Review;
 import lombok.Getter;
 
 @Getter
 public class ReviewCreateResponseDto {
     private Long id;
 
-    public ReviewCreateResponseDto(Long id) {
-        this.id = id;
-    }
-
-    public ReviewRequestDto reviewRequestDto() {
-        return new ReviewRequestDto(id);
+    public ReviewCreateResponseDto(Review review) {
+        this.id = review.getId();
     }
 }
