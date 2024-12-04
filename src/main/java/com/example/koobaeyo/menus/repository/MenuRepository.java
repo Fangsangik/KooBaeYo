b6d059b13dc1541e4deb73ba60e3b5f72c072f15
gyoungsubLee <dltjq2323@gmail.com>
2024-12-04 19:41:58 +0900

    feat: Menu 생성 API

---
+++ src/main/java/com/example/koobaeyo/menus/repository/MenuRepository.java
@@ -0,0 +1,18 @@
+package com.example.koobaeyo.menus.repository;
+
+import com.example.koobaeyo.menus.entity.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+    default Menu findByIdOrElseThrow(Long menuId){
+        return findById(menuId)
+                .orElseThrow(()->{
+                    throw new ResponseStatusException(HttpStatus.NOT_FOUND, "해당하는 메뉴가 존재하지 않습니다.");
+                });
+    };
+
+}
