c42e23de796e7749ababd67fbffb06f4a2a3437d
kmj-23 <kimmj1304@gmail.com>
2024-12-06 14:31:10 +0900

    deletedAt, valid, comment 추가

--- src/main/java/com/example/koobaeyo/user/controller/UserController.java
+++ src/main/java/com/example/koobaeyo/user/controller/UserController.java
@@ -12,6 +12,7 @@ import com.example.koobaeyo.user.entity.User;
 import com.example.koobaeyo.user.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,8 +29,13 @@ public class UserController {
         this.userService = userService;
     }
 
+    /**
+     * 회원가입
+     * @param requestDto - 회원가입을 위한 요청정보
+     * @return SignUpResponseDto - 회원가입 완료 응답 dto
+     */
     @PostMapping("/sign-up")
-    public ResponseEntity<CommonResponse<SignUpResponseDto>> signup(@RequestBody SignUpRequestDto requestDto) {
+    public ResponseEntity<CommonResponse<SignUpResponseDto>> signup(@Valid @RequestBody SignUpRequestDto requestDto) {
 
         SignUpResponseDto signUpResponseDto =
                 userService.signUp(requestDto);
@@ -37,6 +43,11 @@ public class UserController {
         return new ResponseEntity<>(new CommonResponse<>("성공했습니다.", signUpResponseDto), HttpStatus.OK);
     }
 
+    /**
+     * 회원 단건 조회
+     * @param id 회원 조회시 필요한 사용자 식별 id
+     * @return FindUserResponseDto - 회원조회에 대한 응답 dto
+     */
     @GetMapping("/users/{id}")
     public ResponseEntity<CommonResponse<FindUserResponseDto>> findUser(@PathVariable Long id) {
 
@@ -46,7 +57,12 @@ public class UserController {
         return new ResponseEntity<>(new CommonResponse<>("조회 결과입니다", findUserResponseDto), HttpStatus.OK);
     }
 
-    // TODO: ID값 @pathvariable로 넣을지 결정
+    /**
+     * 회원 수정
+     * @param requestDto 회원 수정을 위한 요청정보
+     * @param request 세션 생성을 위한 HTTP요청
+     * @return UpdateUserResponseDto - 회원수정에 대한 응답 dto
+     */
     @PutMapping("/users/{id}")
     public ResponseEntity<CommonResponse<UpdateUserResponseDto>> updateUser(@RequestBody UpdateUserRequestDto requestDto, HttpServletRequest request) {
         HttpSession session = request.getSession(false);
@@ -58,6 +74,13 @@ public class UserController {
         return new ResponseEntity<>(new CommonResponse<>("정상적으로 수정되었습니다", updateUserResponseDto), HttpStatus.CREATED);
     }
 
+    /**
+     * 회원 삭제
+     * @param id 회원 삭제시 필요한 사용자 고유 식별 id
+     * @param deleteUserRequestDto 회원 삭제시 필요한
+     * @param request 세션 생성을 위한 HTTP요청
+     * @return 없음 / 회원삭제 성공 메세지
+     */
     @DeleteMapping("/users/{id}")
     public ResponseEntity<CommonResponse<Void>> deleteUser(@PathVariable Long id, @RequestBody DeleteUserRequestDto deleteUserRequestDto, HttpServletRequest request) {
 
